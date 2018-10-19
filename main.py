6e10808c4c31f990a6b3c5319d9b0b02669d1ac9
teek <prateekm33@gmail.com>
2018-10-19 02:39:38 -0400

    changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ force = True # True to re-run preprocessing step and not use stored data
 
 # Meta variables
 test_size = 0.3 # proportion of overall training set
-sample_size = 100  # number of dps to set as size for original training set
+sample_size = None  # number of dps to set as size for original training set
 batched_sample_size = 50 # number of dps from sample_size to run at once
 epochs = 5
 batch_size = 16
@@ -19,12 +19,10 @@ if bclass == True:
   loss = "binary_crossentropy"
 optimizer = "adam"
 
-_runs = np.int(np.ceil(sample_size / batched_sample_size)) # total number of runs until all of sample_size completed
-
-def main(force=False, skip_pandas=0):
+def main(force=False):
   # Process images
   if force:
-    train_X_orig, train_Y_orig = process_images('stage_1_train_images', 'stage_1_train_labels.csv', sample_size=sample_size, bclass=bclass, batch_size=batch_size, skip=skip_pandas)
+    train_X_orig, train_Y_orig = process_images('stage_1_train_images', 'stage_1_train_labels.csv', sample_size=sample_size, bclass=bclass)
   else:
     train_X_orig, train_Y_orig = load_data()
 
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
 from process_images import process_images
 import numpy as np
 
-images, labels = process_images('stage_1_train_images', 'stage_1_train_labels.csv', sample_size=100)
-
+images, labels = process_images('stage_1_train_images', 'stage_1_train_labels.csv', sample_size=1)
+print('shape of images : ', images.shape)
+print('shape of labels : ', labels.shape)
 # images_file = open('processed_train_images', 'w')
 np.save('processed_train_images', images)
 
