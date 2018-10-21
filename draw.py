f2a4c788f71866c1fa41ef3766eaadcce41fa610
Prateek Madapurmath <prateekmadapurmath@Prateeks-MacBook-Air.local>
2018-10-20 22:05:08 -0700

    update draw.py

--- draw.py
+++ draw.py
@@ -3,25 +3,31 @@ from load_data import load_data
 import pydicom
 
 images, labels = load_data()
-im = images[4]
-label = labels[4]
-x = int(label[1])
-y = int(label[2])
-x2 = x + int(label[3])
-y2 = y + int(label[4])
-cv2.rectangle(im,(x,y),(x2,y2),(0,0,255),3)
-cv2.imshow('image_down', im)
-cv2.waitKey(0)
+orig_labels = pd.read_csv('stage_1_train_labels.csv', dtype={'patientId': str, 'x': np.float32, 'y': np.float32, 'width': np.float32, 'height': np.float32, 'Target': np.float32}, engine="python", nrows=len(labels)).fillna(0).values
 
-ds = pydicom.dcmread('./stage_1_train_images/00436515-870c-4b36-a041-de91049b9ab4.dcm')
-im_orig = ds.pixel_array
-_x = 264
-_y = 152
-_x2 = _x + 213
-_y2 = _y + 379
-cv2.rectangle(im_orig,(_x,_y),(_x2,_y2),(0,0,255),3)
+for i in range(0, len(labels)):
+  im = images[i]
+  label = labels[i]
+  x = int(label[1])
+  y = int(label[2])
+  x2 = x + int(label[3])
+  y2 = y + int(label[4])
+  print(x, y, x2, y2)
+  cv2.rectangle(im,(x,y),(x2,y2),(0,0,255),3)
+  cv2.imshow('image_down', im)
+  cv2.waitKey(0)
 
-cv2.imshow('image_orig', im_orig)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+  ds = pydicom.dcmread('./stage_1_train_images/' + label[0] + '.dcm')
+  im_orig = ds.pixel_array
+  _x = orig_labels[0, 1]
+  _y = orig_labels[0, 2]
+  _x2 = _x + orig_labels[0, 3]
+  _y2 = _y + orig_labels[0, 4]
+  cv2.rectangle(im_orig,(_x,_y),(_x2,_y2),(0,0,255),3)
 
+  cv2.imshow('image_orig', im_orig)
+  key = cv2.waitKey(0)
+  print(key)
+  if key  == 27:
+     cv2.destroyAllWindows()
+     break
