e9e23b82eab9c0c45d855de1cfcfb4f59a0fd842
Prateek Madapurmath <prateekmadapurmath@Prateeks-MacBook-Air.local>
2018-10-21 11:17:39 -0700

    add data generator

---
+++ get_partition.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from os import listdir
+
+'''
+get all IDs -- read from csv
+shuffle/rand perm
+'''
+
+def get_partition():
+  IDs = np.array(pd.read_csv(
+    labels_csv,
+    dtype={'patientId': str, 'x': np.float32, 'y': np.float32, 'width': np.float32, 'height': np.float32, 'Target': np.float32},
+    engine="python",
+    usecols=['patientId']
+  ).values)
+  np.random.shuffle(IDs)
+  partition_1 = np.floor(len(IDs) * 0.6)
+  partition_2 = np.floor(len(IDs) * 0.8)
+  return {
+    'train': IDs[:partition_1,:],
+    'validation': IDs[partition_1:partition_2,:],
+    'test': IDs[:partition_2,:]
+  }
+  
+def get_labels():
+  labels = [np.load(f).label for f in listdir('processed_train_labels')]
+
+  labels_dict = {}
+  for i in range(0, len(labels)):
+    labels_dict[labels[i, 0]] = labels[i, 1:]
+  return labels_dict
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from process_images import process_images
 from model import PneuModel
 from sklearn.model_selection import train_test_split
 from load_data import load_data
+from get_partition import get_partition, get_labels
 
 # Debug variables
 bclass = False # True for binary_classification only. False for classification w/bb's
@@ -27,10 +28,10 @@ optimizer = "adam"
 
 def main(force=False):
   # Process images
-  if force:
-    train_X_orig, train_Y_orig = process_images('stage_1_train_images', 'stage_1_train_labels.csv', sample_size=sample_size, bclass=bclass)
-  else:
-    train_X_orig, train_Y_orig = load_data()
+  # if force:
+  #   train_X_orig, train_Y_orig = process_images('stage_1_train_images', 'stage_1_train_labels.csv', sample_size=sample_size, bclass=bclass)
+  # else:
+  #   train_X_orig, train_Y_orig = load_data()
 
   train_X, test_X, train_Y, test_Y = train_test_split(train_X_orig, train_Y_orig[:, 1:], test_size=0.3)
   print("size of training set X: ", train_X.shape)
@@ -42,7 +43,7 @@ def main(force=False):
   
   # Create model
   pneuModel = PneuModel(train_X.shape[1:], bclass=bclass)
-  
+
   # # Compile model
   pneuModel.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
 
@@ -56,5 +57,49 @@ def main(force=False):
   print ("Loss = " + str(preds[0]))
   print ("Test Accuracy = " + str(preds[1]))
 
-main(force)
 
+
+
+def run():
+  params = {'dim': (600,600,1),
+          'batch_size': 16,
+          'n_classes': 5,
+          'n_channels': 1,
+          'shuffle': True}
+
+  # Datasets
+  partition = get_partition() # IDs
+  labels = get_labels() # Labels
+
+  # Generators
+  training_generator = DataGenerator(partition['train'], labels, **params)
+  validation_generator = DataGenerator(partition['validation'], labels, **params)
+
+  # Create model
+  pneuModel = PneuModel(params.dim, bclass=bclass)
+
+  # Compile model
+  pneuModel.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
+
+  # Train model on dataset
+  pneuModel.fit_generator(generator=training_generator,
+                      validation_data=validation_generator,
+                      use_multiprocessing=True,
+                      workers=6)
+  
+  # Test model
+  test_label_ids = partition['test']
+  test_Y = np.zeros((len(test_label_ids, 5)))
+  test_X = np.zeros((len(test_label_ids, 600, 600, 1)))
+  for i in range(len(test_label_ids)):
+    _id = test_label_ids[i]
+    test_Y[i] = labels[_id]
+    test_X[i] = np.load('processed_train_images/' + _id + '.npz').image
+
+  preds = pneuModel.evaluate(x=test_X, y=test_Y)
+
+  print()
+  print ("Loss = " + str(preds[0]))
+  print("Test Accuracy = " + str(preds[1]))
+  
+run()
