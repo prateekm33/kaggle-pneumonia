bc69b84df7da3e656b4a3aaac66dd74b84715bfd
Prateek Madapurmath <prateekmadapurmath@Prateeks-MacBook-Air.local>
2018-10-21 21:31:27 -0700

    add resize fn

--- process_images.py
+++ process_images.py
@@ -5,14 +5,19 @@ import pydicom
 import scipy.misc
 import cv2
 
-def convertDCM_PNG(dir, file):
+def resizeIMG(arr, size=(200,200)):
+  img = cv2.resize(arr, size)
+  return np.reshape(img, (*size, 1))
+
+def resizeDCM(dir, file):
   ds = pydicom.dcmread(dir + '/' + file)
   ds_ = np.array(ds.pixel_array)
-  ds_ = cv2.resize(ds_, (600,600)) / 255
-  ds_ = np.reshape(ds_, (600,600,1))
-  shape = ds.pixel_array.shape
+  # ds_ = cv2.resize(ds_, (600,600)) / 255
+  # ds_ = np.reshape(ds_, (600, 600, 1))
+  ds_ = resizeIMG(ds_)
+  orig_shape = ds.pixel_array.shape
 
-  return ds_, shape
+  return ds_, orig_shape
 
 def process_images(images_dir, labels_csv,  sample_size=None, bclass = False):
   labels = pd.read_csv(labels_csv, dtype={'patientId': str, 'x': np.float32, 'y': np.float32, 'width': np.float32, 'height': np.float32, 'Target': np.float32}, engine="python", nrows=sample_size).fillna(0).values
@@ -25,7 +30,7 @@ def process_images(images_dir, labels_csv,  sample_size=None, bclass = False):
   # images = np.zeros((len(labels), 600, 600, 1))
   for i in range(0, len(labels)):
     f = labels[i, 0] + '.dcm'
-    image, shape = convertDCM_PNG(images_dir, f)
+    image, shape = resizeDCM(images_dir, f)
     # images[i] = image
     np.savez_compressed('processed_train_images/' + labels[i, 0], image=image)
     if bclass == False:
